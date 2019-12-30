d9749099ab2c25ae1766a88c72e100d71ced5a58
Rachel Gould <rachel@rachelgould.dev>
2019-12-29 23:33:51 -0800

    intial gatsby implementation and basic layout

---
+++ gatsby/src/components/layout.js
@@ -0,0 +1,67 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import "./layout.css"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+const Layout = ({ children }) => {
+  const classes = useStyles();
+
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton> */}
+            <Typography variant="h6" color="inherit">
+              Your Pixels Dashboard
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <main>{children}</main>
+      <footer>
+        <a href="https://rachelgould.dev">Made by Rachel Gould</a>
+      </footer>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,50 @@
+import React from "react"
+import { Link } from "gatsby"
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Layout from "../components/layout"
+// import Image from "../components/image"
+import SEO from "../components/seo"
+import 'typeface-roboto';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+
+const IndexPage = () => {
+  const classes = useStyles();
+  return (
+    <Layout>
+      <SEO title="Home" />
+        <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>xs</Paper>
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs>
+            <Paper className={classes.paper}>xs</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>xs=6</Paper>
+          </Grid>
+          <Grid item xs>
+            <Paper className={classes.paper}>xs</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </Layout>
+  )
+}
+
+export default IndexPage
